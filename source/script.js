e3e47856a73457e20276d55683d6627693fe3d95
M. Fahrianor <moraxz112@gmail.com>
2025-09-27 02:04:40 +0800

    penambahan js

--- source/script.js
+++ source/script.js
@@ -1,33 +1,3 @@
-// Validasi Form Login
-function showError() {
-    const form = document.querySelector("form");
-    form.style.animation = "shake 0.3s";
-    setTimeout(() => form.style.animation = "", 300);
-}
-
-function validasi() {
-    const username = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value.trim();
-    const errorMsg = document.getElementById("errorMsg");
-
-
-    errorMsg.textContent = "";
-
-    if (username === "" || password === "") {
-        errorMsg.textContent = "Username dan Password wajib diisi!";
-        showError();
-        return false;    
-    }
-
-    if (password.length < 6) {
-        errorMsg.textContent = "Password minimal 6 karakter!";
-        showError();
-        return false;
-    }
-
-    return false; 
-}
-
 // Form Hubungi Kami
 const hubungiForm = document.querySelector('.hubungi');
 
